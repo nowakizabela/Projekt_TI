8dde44f8aa8839284891929440d11479800fe854
nowakizabela <72295241+nowakizabela@users.noreply.github.com>
2023-07-07 20:38:21 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,64 @@
+function Add_user() {
+    var r = document.getElementById("new_user");
+    if (r.style.display === "block") {
+        r.style.display = "none";
+    } else {
+        r.style.display = "block";
+    }
+}
+function Cancel_add_user() {
+    var cr = document.getElementById("new_user");
+    if (cr.style.display === "none") {
+        cr.style.display = "block";
+    } else {
+        cr.style.display = "none";
+    }
+}
+function Cancel_pick_user() {
+    var cr = document.getElementById("choose_role");
+    if (cr.style.display === "none") {
+        cr.style.display = "block";
+    } else {
+        cr.style.display = "none";
+    }
+}
+function Cancel_delete_user() {
+    var cr = document.getElementById("delete_user");
+    if (cr.style.display === "none") {
+        cr.style.display = "block";
+    } else {
+        cr.style.display = "none";
+    }
+}
+function Add_book() {
+    var b = document.getElementById("new_book");
+    if (b.style.display === "block") {
+        b.style.display = "none";
+    } else {
+        b.style.display = "block";
+    }
+}
+function Cancel_add_book() {
+    var bc = document.getElementById("new_book");
+    if (bc.style.display === "none") {
+        bc.style.display = "block";
+    } else {
+        bc.style.display = "none";
+    }
+}
+function Cancel_pick_book() {
+    var cr = document.getElementById("change_book");
+    if (cr.style.display === "none") {
+        cr.style.display = "block";
+    } else {
+        cr.style.display = "none";
+    }
+}
+function Cancel_delete_book() {
+    var cr = document.getElementById("delete_book");
+    if (cr.style.display === "none") {
+        cr.style.display = "block";
+    } else {
+        cr.style.display = "none";
+    }
+}
